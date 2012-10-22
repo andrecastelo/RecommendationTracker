778c5f78374995ab8104a138ec352e6cf713df3e
André Castelo <andrecastelo@gmail.com>
2012-10-22 16:08:47 -0200

    Added admin table, in order to not allow unauthorized access to tamper with the database.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 drop table if exists colaboradores;
 drop table if exists clientes;
+drop table if exists admins;
 create table colaboradores (
 	id INTEGER PRIMARY KEY autoincrement,
 	nome string not null
@@ -14,8 +15,17 @@ create table clientes (
 	FOREIGN KEY(indicacao) REFERENCES colaboradores(id)
 );
 
+create table admins (
+    id INTEGER PRIMARY KEY autoincrement,
+    nome STRING NOT NULL,
+    password STRING NOT NULL
+);
+
 insert into colaboradores (nome) values ('agomes');
 insert into colaboradores (nome) values ('fnery');
 insert into colaboradores (nome) values ('llisboa');
 insert into colaboradores (nome) values ('nsixel');
 insert into colaboradores (nome) values ('vdias');
+
+insert into admins (nome, password) values ('andrecastelo', 'c3d05c5fe71edf6960934a802ca1bf4257404646');
+insert into admins (nome, password) values ('nsixel', '637dd3894df6b16a956ab46ed377e5d85a7b2310');
